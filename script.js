956fcf9918a6d82dd4c1a5a164f45cdd4105a4ab
Phil Miranthis <philippe.miranthis@gmail.com>
2024-11-19 19:55:52 +0000

    feat: add support for Jokers in card game and persist user preference for including Jokers

--- script.js
+++ script.js
@@ -1,37 +1,51 @@
 const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
 const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+const jokers = ['Red Joker', 'Black Joker'];
 
+// Card class representing a single card
 class Card {
     constructor(suit, rank) {
         this.suit = suit;
         this.rank = rank;
     }
 
+    // Get the value of the card based on its rank
     getValue() {
+        if (this.rank === 'Joker') return 0; // Assign a special value for Jokers
         return ranks.indexOf(this.rank) + 2;
     }
 
+    // Return a string representation of the card
     toString() {
         return `${this.rank} of ${this.suit}`;
     }
 }
 
+// Deck class representing a deck of cards
 class Deck {
-    constructor() {
+    constructor(includeJokers) {
         this.cards = [];
-        this.initializeDeck();
+        this.initializeDeck(includeJokers);
     }
 
-    initializeDeck() {
+    // Initialize the deck with 52 cards (and optionally Jokers)
+    initializeDeck(includeJokers) {
         this.cards = [];
         for (let suit of suits) {
             for (let rank of ranks) {
                 this.cards.push(new Card(suit, rank));
             }
         }
+        if (includeJokers) {
+            for (let joker of jokers) {
+                this.cards.push(new Card('None', joker)); // Add two Jokers
+                this.cards.push(new Card('None', joker));
+            }
+        }
         this.shuffle();
     }
 
+    // Shuffle the deck
     shuffle() {
         for (let i = this.cards.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
@@ -39,15 +53,20 @@ class Deck {
         }
     }
 
+    // Deal a card from the deck
     deal() {
         return this.cards.pop();
     }
 }
 
-let deck = new Deck();
+// Read the includeJokers setting from localStorage
+const includeJokers = localStorage.getItem('includeJokers') === 'true';
+
+// Initialize the deck and deal the first card
+let deck = new Deck(includeJokers);
 let currentCard = deck.deal();
-let correctGuesses = 0; // Initialize the count of correct guesses
-let totalGames = 0; // Initialize the count of total games played
+let correctGuesses = 0;
+let totalGames = 0;
 
 $(document).ready(function() {
     // Set the initial card image when the document is ready
@@ -62,9 +81,20 @@ $(document).ready(function() {
 
 // Function to get the image path for a given card
 function getCardImage(card) {
-    const rank = card.rank.toLowerCase();
-    const suit = card.suit.toLowerCase();
-    return `PNG-cards-1.3/${rank}_of_${suit}.png`;
+    if (card.rank == 'Black Joker'){
+            card_filename = "black_joker.png"
+    }
+
+    if (card.rank == 'Red Joker'){
+            card_filename = "red_joker.png"
+    }
+
+    else {
+        const rank = card.rank.toLowerCase();
+        const suit = card.suit.toLowerCase();
+        card_filename = `${rank}_of_${suit}.png`
+    }
+    return `PNG-cards-1.3/${card_filename}`;
 }
 
 // Function to display a message for 5 seconds
